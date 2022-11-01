b6dfe374b130e0d26583c6db4abe2118412b1a8c
jNathancodes <absolut.kalinka@gmail.com>
2022-11-01 21:04:09 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './style.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ContactsView from './views/ContactsView';
+import HomeView from './views/HomeView';
+import NotFoundView from './views/NotFoundView';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"element={<HomeView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

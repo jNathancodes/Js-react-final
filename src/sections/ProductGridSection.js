e88727202c86f62b5ed0832480428d03d245301a
jNathancodes <absolut.kalinka@gmail.com>
2022-11-07 17:32:21 +0100

    useState-done

--- src/sections/ProductGridSection.js
+++ src/sections/ProductGridSection.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ProductCard from '../assets/scss/components/ProductCard'
 
 
 
-const ProductGridSection = ({title}) => {
+const ProductGridSection = ({title, products}) => {
 
-  const [products, setProducts] = useState([
-    { id: 1, productName: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, Img: "card-Img.jpg" },
-    { id: 2, productName: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, Img: "card-Img.jpg" },
-    { id: 3, productName: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, Img: "card-Img.jpg" },
-    { id: 4, productName: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, Img: "card-Img.jpg" }
-  ])
+
   return (
 <section className="product-grid">
     <h1>{title}</h1> 
         <div className="container">
-            <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div className="row row-cols-1 row-cols-md-4 g-4">
               {
-                products.map(products => <ProductCard />)
+                products.map(product => <ProductCard key={product.id} product={product} />)
               }
 
             </div>
